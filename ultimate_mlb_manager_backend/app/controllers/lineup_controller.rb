31dbddf97254089eaa05d87090a4e2345c993b93
pbedrosian <peter_bedrosian@yahoo.com>
2020-09-11 11:10:38 -0400

    Strong pereams for lineup

--- ultimate_mlb_manager_backend/app/controllers/lineup_controller.rb
+++ ultimate_mlb_manager_backend/app/controllers/lineup_controller.rb
@@ -9,9 +9,28 @@ class LineupController < ApplicationController
         render json: @lineup
     end
 
+    def create
+        new_lineup = Lineup.new(lineup_params)
+        new_lineup.save
+    end
+
     private
 
     def get_lineup
         @lineup = Lineup.find_by_id(params[:id])
+    end
+
+    def lineup_params
+        params.require(:lineup).permit(
+            :first_player_id,
+            :second_player_id,
+            :third_player_id,
+            :fourth_player_id,
+            :fifth_player_id,
+            :sixth_player_id,
+            :seventh_player_id,
+            :eighth_player_id,
+            :nineth_layer_id
+        )
     end
 end
