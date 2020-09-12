00f26b104de1623ad76c1fd3f392536edeff15f7
pbedrosian <peter_bedrosian@yahoo.com>
2020-09-12 13:22:24 -0400

    Start of adding player cards

--- ultimate_mlb_manager_backend/app/serializers/player_serializer.rb
+++ ultimate_mlb_manager_backend/app/serializers/player_serializer.rb
@@ -1,5 +1,5 @@
 class PlayerSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :primary_position, :number, :bats, :throws, :image_url
   has_many :stats
-  has_many :lineups
+  # has_many :lineups
 end
---
+++ ultimate_mlb_manager_frontend/src/player.js
@@ -0,0 +1,5 @@
+class Player {
+    name() {
+        `${self.first_name} ${self.last_name}`
+    }
+}
